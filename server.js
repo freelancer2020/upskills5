34f45b97f4ef5a41f9c3b178ed480c9649e32485
Mostafa Mohamed (PGS Software) <mmohamed@pgs-soft.com>
2022-08-19 14:28:42 +0200

    rc

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(` App running on port ${port}`));
+
+app.use(express.static("./client/rc/build"));

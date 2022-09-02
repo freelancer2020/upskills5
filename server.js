6ce94720b969644e09cedb7500bfe81d48ab6773
Mostafa Mohamed (PGS Software) <mmohamed@pgs-soft.com>
2022-09-02 12:43:14 +0200

    get nodes

--- server.js
+++ server.js
@@ -6,5 +6,16 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(` App running on port ${port}`));
 
-app.use(express.static("./client/rc/build"));
+// app.use(express.static("./client/rc/build"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/rc/build"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/rc/build/index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}

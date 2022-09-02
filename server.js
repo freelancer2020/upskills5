f63b2a46702a17f09bff25d1d32eb5f7c965b0c2
Mostafa Mohamed (PGS Software) <mmohamed@pgs-soft.com>
2022-09-02 12:40:44 +0200

    static

--- server.js
+++ server.js
@@ -6,8 +6,5 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(` App running on port ${port}`));
 
-// app.use(express.static("./client/rc/build"));
+app.use(express.static("./client/rc/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/rc/build"));
-});

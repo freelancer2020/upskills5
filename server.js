eec85d8991f4a472998a04f958e76df5d6739bf5
Mostafa Mohamed (PGS Software) <mmohamed@pgs-soft.com>
2022-09-05 06:55:02 +0200

    fix bug

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import path from "path";
-// import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP } from "express-graphql";
 
 //graphql schema and resolvers
-// import schema from "./graphQL/schema.js";
-// import resolvers from "./graphQL/resolvers.js";
+import schema from "./graphQL/schema.js";
+import resolvers from "./graphQL/resolvers.js";
 
 const __dirname = path.resolve();
 
 const app = express();
 
-// const graphQL = graphqlHTTP({
-//   schema: schema,
-//   rootValue: resolvers,
-//   graphiql: true,
-// });
+const graphQL = graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true,
+});
 
 const port = process.env.PORT || 8000;
 
@@ -22,7 +22,7 @@ app.listen(port, () => console.log(` App running on port ${port}`));
 
 app.use(express.json());
 app.use(express.static("./client/rc/build"));
-// app.use("/graphql/claimData", graphQL);
+app.use("/graphql/claimData", graphQL);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/rc/build/index.html"));
